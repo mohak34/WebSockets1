5d16a4daa49e024152b93ea0e73fabe93efc86f8
Mohak S <mohaks0408@gmail.com>
2023-12-17 14:23:45 +0530

    Added activity listening

--- server/index.js
+++ server/index.js
@@ -28,8 +28,31 @@ const io = new Server(expressServer, {
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
+  //Upon connection to connected user
+  socket.emit("message", `Welcome to the chat appplication`);
+
+  //Upon connection to all connected users
+  socket.broadcast.emit(
+    "message",
+    `User ${socket.id.substring(0, 5)} connected`
+  );
+
+  //Listening for a message event
   socket.on("message", (data) => {
     console.log(data);
     io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
+  });
+
+  //When user disconnects - to all others
+  socket.on("disconnect", () => {
+    socket.broadcast.emit(
+      "message",
+      `User ${socket.id.substring(0, 5)} disconnected`
+    );
+  });
+
+  //Listening for activity
+  socket.on("activity", (name) => {
+    socket.broadcast.emit("activity", name);
   });
 });
